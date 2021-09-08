13bdac04bddaff8181a8311d51e2c5cfdfeb3314
SantiagoJSG <santiagojsanchezg@gmail.com>
2021-09-08 00:12:41 -0500

    Ultimos cambios.

--- Parcial “OnlyPans”/Admin.Designer.cs
+++ Parcial “OnlyPans”/Admin.Designer.cs
@@ -29,19 +29,34 @@ namespace Parcial__OnlyPans_
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.label11 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Oswald Medium", 50F, System.Drawing.FontStyle.Bold);
+			this.label11.Location = new System.Drawing.Point(26, 157);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(749, 114);
+			this.label11.TabIndex = 20;
+			this.label11.Text = "¡Bienvenido Administrador!";
 			// 
 			// Admin
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label11);
 			this.Name = "Admin";
 			this.Text = "Admin";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label11;
 	}
 }
